3233589b4edbe6602ffbc3097763573ae8bf3b30
Netta Richer <nettaricher1@gmail.com>
2019-04-29 20:29:17 +0300

    merging2

--- src/screens/Fullscreen.js
+++ src/screens/Fullscreen.js
@@ -106,7 +106,7 @@ class FullScreen extends Component {
                     <Button
                         onPress={this.handleReturn}
                         title="Go Back"
-                        color="#393e43"
+                        color="#346dad"
                         accessibilityLabel="Return to home page"
                     />
                 </View>
